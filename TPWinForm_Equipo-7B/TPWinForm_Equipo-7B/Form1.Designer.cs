071a259480c15d3504dafcc2713fb32895a046ef
Jaskeir <rojasezequielsebastian@hotmail.com>
2025-04-10 21:08:45 -0300

    1ra VENTANA

--- TPWinForm_Equipo-7B/TPWinForm_Equipo-7B/Form1.Designer.cs
+++ TPWinForm_Equipo-7B/TPWinForm_Equipo-7B/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(286, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CATALOGO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "BUSCAR PRODUCTO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GESTION DE ARTICULOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
