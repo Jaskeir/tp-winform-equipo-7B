1504149b5b3b7fa911f826d0362e4a5163157bc2
Salchy <leandrocg524@gmail.com>
2025-04-20 12:37:48 -0300

    añadido imagenesDatos

---
+++ TPWinForm_Equipo-7B/datos/imagenesDatos.cs
@@ -0,0 +1,43 @@
+﻿using ConexionDB;
+using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datos
+{
+    public class imagenesDatos
+    {
+        public List<Imagen> Listar(int id)
+        {
+            List<Imagen> lista = new List<Imagen>();
+
+            Admin_Datos database = new Admin_Datos();
+
+            try
+            {
+                database.setearConsulta("SELECT ImagenUrl FROM Imagenes WHERE IdArticulo = @id");
+                database.setearParametro("@id", id);
+                database.ejecutarLectura();
+
+                while (database.Lector.Read())
+                {
+                    lista.Add(new Imagen((string)database.Lector["ImagenUrl"]));
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                database.cerrarConexion();
+            }
+            return lista;
+        }
+    }
+}
